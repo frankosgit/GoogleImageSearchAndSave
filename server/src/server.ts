00ce219cf705f65a521e18270ab454c29d938de1
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-03-07 16:09:08 +0100

    updates to server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,10 +6,39 @@ import mongoose from 'mongoose';
 const router = express()
 
 mongoose
-.connect(config.mongo.url)
-.then(() => {
-    console.log('connected')
-})
-.catch((err) => {
-    console.log(err)
-})
+    .connect(config.mongo.url)
+    .then(() => {
+        console.log('Connected to mongoDB')
+        startServer()
+    })
+    .catch((err) => {
+        console.log("Unable to connect to:")
+        console.log(err)
+    });
+
+
+const startServer = () => {
+    router.use((req, res, next) => {
+        console.log(`Incoming method: [${req.method}] - url: [${req.url}] IP: [${req.socket.remoteAddress}]`)
+
+        res.on('finish', () => {
+            console.log(`Incoming method: [${req.method}] - url: [${req.url}] IP: [${req.socket.remoteAddress}] - status: [${res.statusCode}]`)
+        })
+
+        next();
+    })
+
+    router.use(express.urlencoded({ extended: true }))
+    router.use(express.json())
+
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if(req.method == 'OPTIONS'){
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GEt');
+            return res.status(200).json({})
+        }
+        next()
+    })
+}
