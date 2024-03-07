73900f8018fc4f2a5c5baed03da21b3e6b8df01c
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-03-07 13:55:23 +0100

    connecting to mongodb

--- imagesearchandsave/src/components/Hero.tsx
+++ imagesearchandsave/src/components/Hero.tsx
@@ -33,10 +33,12 @@ const Hero = () => {
     return (
         <div className='w-screen h-screen mx-auto px-8 pt-[6.4rem] bg-secondary text-center'>
             <div>
+
                 <h2 className='text-3xl'>Search the internet for photos</h2>
                 <h3 className='text-xl my-3'>and add them to your hive</h3>
+                <img className='w-1/12 mx-auto mt-4' src='../public/hivephoto.png' />
                 <input onChange={onChange} type="text" placeholder="Type here" onKeyDown={handleKeyDown}
- className="input input-bordered input-primary w-full max-w-xs mb-12" />
+                    className="input input-bordered input-primary w-full max-w-xs mb-12" />
                 {/* <span className="loading loading-infinity loading-lg"></span> */}
                 <button
                     className="ml-6 btn btn-m btn-outline btn-primary text-m"
